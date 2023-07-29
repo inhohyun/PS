74de12ff1f3556290052c2f694675f15756104d3
인호현 <96523102+inhohyun@users.noreply.github.com>
2023-07-29 22:24:57 +0900

    add : inhohyun 14503.py

--- 인호현/삼성기출/14503.py
+++ 인호현/삼성기출/14503.py
@@ -1,17 +1,47 @@
-# 0 : 위, 1 : 오른쪽, 2 : 아래, 3 : 왼쪽
-# 0 : 청소되지 않음, 1 : 벽
-
-n, m = map(int, input().split())
-r, c, d = map(int, input().split())
-
-room = []
-for i in range(n):
-    room.append(list(map(int, input().split())))
+import sys
 
 
-while True:
-    # 현재 위치 청소
-    room[r][c] = 1
-    
+def dfs(x, y, v):
+    global answer
 
-# 지나간 자리를 전부 1로 만들면서 진행
+    # 빈 곳이면 청소
+    if graph[x][y] == 0:
+        graph[x][y] = 2 # 방문 처리
+        answer += 1 # 청소 구역 카운트
+
+    # 반복문을 통해 4방향 확인
+    for _ in range(4):
+        nv = (v + 3) % 4 # 현재 방향 + 3을 4로 나눈 나머지가 왼쪽 방향
+        nx = x + dx[nv]
+        ny = y + dy[nv]
+
+        # 이동 위치가 빈 곳이면 탐색
+        if graph[nx][ny] == 0:
+            dfs(nx, ny, nv)
+            return
+        # 현재 방향 변경
+        v = nv
+
+    # 4방향 모두 탐색했다면
+    nv = (v + 2) % 4 # 현재방향 + 2를 4로 나눈 나머지가 후진 방향
+    nx = x + dx[nv]
+    ny = y + dy[nv]
+
+    # 이동 위치가 벽이라면 리턴
+    if graph[nx][ny] == 1:
+        return
+
+    # 이동 위치가 벽이 아니라면 탐색
+    dfs(nx, ny, v)
+
+
+n, m = map(int, sys.stdin.readline().split())
+r, c, d = map(int, sys.stdin.readline().split())
+graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+
+# d => 0,3,2,1 순서로 돌아야함. 북/서/남/동
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+answer = 0
+dfs(r, c, d)
+print(answer)
