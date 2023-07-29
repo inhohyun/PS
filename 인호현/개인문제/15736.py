9fd89e583fae3164a96c26a36cea5b338128d44b
인호현 <96523102+inhohyun@users.noreply.github.com>
2023-07-29 17:32:35 +0900

    add : inhohyun 14503.py

--- 인호현/개인문제/15736.py
+++ 인호현/개인문제/15736.py
@@ -5,15 +5,15 @@ n = int(input())
 answer = int(n **0.5)
 print(answer)
 
-# 0은 청, 백은 1
-flag = [0] * (n+1)
-flag [0] = 10
-for i in range(1, n+1):
-    for j in range(i, n, i):
-        if flag[j] == 0:
-            flag[j] = 1
-        elif flag[j] == 1:
-            flag[j] = 0
+# # 0은 청, 백은 1
+# flag = [0] * (n+1)
+# flag [0] = 10
+# for i in range(1, n+1):
+#     for j in range(i, n, i):
+#         if flag[j] == 0:
+#             flag[j] = 1
+#         elif flag[j] == 1:
+#             flag[j] = 0
 
-print(flag.count(1))  
+# print(flag.count(1))  
 
