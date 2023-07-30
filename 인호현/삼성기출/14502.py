04ac07b33e5dd51ff7b3f883881f35dcfd940ca9
인호현 <96523102+inhohyun@users.noreply.github.com>
2023-07-30 13:05:09 +0900

    add : inhohyun 14502.py

--- 인호현/삼성기출/14502.py
+++ 인호현/삼성기출/14502.py
@@ -1,9 +1,49 @@
-n,m = map(int, input().split())
+import sys
+import copy
+from collections import deque
 
-lab = []
-for i in range(n):
-    lab.append(list(map(int, input().split())))
+input = sys.stdin.readline
 
-# 입력값이 적음 -> 전부다 완탐으로 진행 가능
-# dfs로 임의로 1을 3개 바꿔가며 지정하고 2근처를 모두 2로 바꿈
-# 0의 개수가 제일 많은것을 출력
+dx = [0,0,-1,1]
+dy = [1,-1,0,0]
+
+N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+ans = 0
+q = deque()
+
+def bfs():
+    global ans
+    w = copy.deepcopy(arr)
+    for i in range(N):
+        for j in range(M):
+            if w[i][j]==2:
+                q.append([i,j])
+
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < N and 0 <= ny < M:
+                if w[nx][ny]==0:
+                    w[nx][ny] = 2
+                    q.append([nx,ny])
+    cnt = 0
+    for i in w:
+        cnt+=i.count(0)
+    ans = max(ans,cnt)
+
+def wall(x):
+    if x==3:
+        bfs()
+        return
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j]==0:
+                arr[i][j]=1
+                wall(x+1)
+                arr[i][j]=0
+
+wall(0)
+print(ans)
